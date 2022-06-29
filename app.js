0fe3a7e42ff26087d4eba11574efd5349f56ef06
Sebastian <106616243+sebastianpenix@users.noreply.github.com>
2022-06-28 22:50:30 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const homeBtn = document.querySelector('.premier-league');
+const arsBtn = document.querySelector('.arsenal');
+const avlBtn = document.querySelector('.aston-villa');
+const bouBtn = document.querySelector('.bournemouth'); 
+const breBtn = document.querySelector('.brentford');
+const bhaBtn = document.querySelector('.brighton-hove-albion');
+const cheBtn = document.querySelector('.chelsea');
+const cryBtn = document.querySelector('.crystal-palace');
+const eveBtn = document.querySelector('.everton');
+const fulBtn = document.querySelector('.fulham');
+const leeBtn = document.querySelector('.leeds');
+const leiBtn = document.querySelector('.leicester-city');
+const livBtn = document.querySelector('.liverpool');
+const mciBtn = document.querySelector('.manchester-city');
+const munBtn = document.querySelector('.manchester-united');
+const newBtn = document.querySelector('.newcastle');
+const nfoBtn = document.querySelector('.nottingham-forrest');
+const souBtn = document.querySelector('.southampton');
+const totBtn = document.querySelector('.tottenham');
+const whuBtn = document.querySelector('.west-ham');
+const wolBtn = document.querySelector('.wolverhampton');
+
+
+document.getElementsByClassName('.arsenal').onclick = function () {
+    location.href= "/.arsenal.html"
+}
